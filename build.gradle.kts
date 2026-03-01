2f95f47ac0be829e033c560d559f3ef29db5bb54
juergen69 <juergen.unfried@gmail.com>
2026-03-01 12:46:02 +0100

    changes by Gemini Autofix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.plugin.compose) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
 }
